f2036b3518199f4e57e54c9d6d431bdfb46c857f
cloudrising <qingmingnet@163.com>
2022-08-17 10:36:52 +0800

    【新增】 测试代码：模块追加前台路由

--- main.go
+++ main.go
@@ -10,22 +10,43 @@
 package sms
 
 import (
+	"fmt"
 	"github.com/gef"
 	"github.com/gef/sms/Events"
 	"github.com/gef/sms/Registry"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
 func Init(g *gef.Gef) {
-
+	
 	//!数据库自动维护
 	dbm := gef.DbManager{}
 	//维护表结构
 	dbm.AutoTable(tables)
 	//维护后台菜单
 	dbm.AutoAdminRules(adminRules)
-
+	
 	//!注册后台页面
 	g.SetAdminPages(Registry.AdminPages)
 	//!注册监听事件
 	g.SetEvent(Events.Listeners)
+	
+	//!测试追加前台路由
+	g.AddFrontRouters([]gef.FrontRouter{
+		{
+			Method:             "GET",
+			Url:                "/sms_test",
+			HandleOrFileSystem: Api(Hello),
+		},
+	})
+}
+
+func Api(next httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		next(w, r, ps)
+	}
+}
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "SMS安装成功")
 }
