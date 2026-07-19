55fc809e670672b2e904c7ec3d4275a81e28339c
qingmingnet <qingmingnet@163.com>
2026-07-19 10:19:35 +0800

    gef.ID改mydb

--- database-tables.go
+++ database-tables.go
@@ -9,7 +9,9 @@
 
 package sms
 
-import "github.com/yqstech/gef"
+import (
+	"github.com/yqstech/gef/dbManager/mydb"
+)
 
 // tables 需要维护的所有的表结构体
 var tables = []interface{}{
@@ -34,11 +36,11 @@ var tables = []interface{}{
 
 // TbSmsUpstream 短信通道表
 type TbSmsUpstream struct {
-	gef.ID
+	mydb.ID
 	UpstreamName string `gorm:"column:upstream_name;type:varchar(20);default:'';NOT NULL;comment:通道名称" json:"upstream_name"` // 通道名称
 	EventName    string `gorm:"column:event_name;type:varchar(50);default:'';NOT NULL;comment:关联事件" json:"event_name"`       // 关联事件
 	IndexNum     int    `gorm:"column:index_num;type:int(11);default:200;NOT NULL;comment:优先级" json:"index_num"`             // 优先级
-	gef.CUSD
+	mydb.CUSD
 }
 
 func (m *TbSmsUpstream) TableName() string {
@@ -47,11 +49,11 @@ func (m *TbSmsUpstream) TableName() string {
 
 // TbSmsUpstreamParams 短信通道配置项
 type TbSmsUpstreamParams struct {
-	gef.ID
+	mydb.ID
 	UpstreamID int    `gorm:"column:upstream_id;type:int(11);default:0;NOT NULL;comment:通道ID" json:"upstream_id"`      // 通道ID
 	ParamName  string `gorm:"column:param_name;type:varchar(30);default:'';NOT NULL;comment:参数名称" json:"param_name"`   // 参数名称
 	ParamTitle string `gorm:"column:param_title;type:varchar(30);default:'';NOT NULL;comment:参数标题" json:"param_title"` // 参数标题
-	gef.CUSD
+	mydb.CUSD
 }
 
 func (m *TbSmsUpstreamParams) TableName() string {
@@ -60,12 +62,12 @@ func (m *TbSmsUpstreamParams) TableName() string {
 
 // TbSmsTemplate 短信模板表
 type TbSmsTemplate struct {
-	gef.ID
+	mydb.ID
 	TemplateName   string `gorm:"column:template_name;type:varchar(30);default:'';NOT NULL;comment:模板标识符" json:"template_name"`       // 模板标识符
 	TemplateTitle  string `gorm:"column:template_title;type:varchar(30);default:'';NOT NULL;comment:模板名称" json:"template_title"`      // 模板名称
 	TemplateVars   string `gorm:"column:template_vars;type:varchar(200);default:'';NOT NULL;comment:模板支持变量" json:"template_vars"`     // 模板支持变量
 	DefaultContent string `gorm:"column:default_content;type:varchar(200);default:'';NOT NULL;comment:默认模板内容" json:"default_content"` // 默认模板内容
-	gef.CUSD
+	mydb.CUSD
 }
 
 func (m *TbSmsTemplate) TableName() string {
@@ -74,12 +76,12 @@ func (m *TbSmsTemplate) TableName() string {
 
 // TbAppSmsUpstream 应用短信通道列表
 type TbAppSmsUpstream struct {
-	gef.ID
+	mydb.ID
 	UniAppId   int    `gorm:"column:uniapp_id;type:int(11);default:0;NOT NULL;comment:应用ID|账号所属小程序" json:"uniapp_id"`
 	UpstreamID int    `gorm:"column:upstream_id;type:int(11);default:0;NOT NULL" json:"upstream_id"`
 	Configs    string `gorm:"column:configs;type:varchar(5000);default:{};NOT NULL;comment:应用拓展配置项" json:"configs"` // 应用拓展配置项
 	IndexNum   int    `gorm:"column:index_num;type:int(11);default:200;NOT NULL;comment:排序" json:"index_num"`       // 排序
-	gef.CUSD
+	mydb.CUSD
 }
 
 func (m *TbAppSmsUpstream) TableName() string {
@@ -88,12 +90,12 @@ func (m *TbAppSmsUpstream) TableName() string {
 
 // TbAppSmsTemplate 应用短信模板
 type TbAppSmsTemplate struct {
-	gef.ID
+	mydb.ID
 	UniAppId        int    `gorm:"column:uniapp_id;type:int(11);default:0;NOT NULL;comment:应用ID|账号所属小程序" json:"uniapp_id"`
 	TemplateName    string `gorm:"column:template_name;type:varchar(30);default:'';NOT NULL;comment:模板标识" json:"template_name"`          // 模板标识
 	TemplateOutID   string `gorm:"column:template_out_id;type:varchar(30);default:'';NOT NULL;comment:外部模板ID" json:"template_out_id"`    // 外部模板ID
 	TemplateContent string `gorm:"column:template_content;type:varchar(200);default:'';NOT NULL;comment:短信模板内容" json:"template_content"` // 短信模板内容
-	gef.CUSD
+	mydb.CUSD
 }
 
 func (m *TbAppSmsTemplate) TableName() string {
@@ -102,7 +104,7 @@ func (m *TbAppSmsTemplate) TableName() string {
 
 // TbAppSmsRecord 应用短信记录
 type TbAppSmsRecord struct {
-	gef.ID
+	mydb.ID
 	UniAppId      int    `gorm:"column:uniapp_id;type:int(11);default:0;NOT NULL;comment:应用ID|账号所属小程序" json:"uniapp_id"`
 	TemplateName  string `gorm:"column:template_name;type:varchar(30);default:'';NOT NULL;comment:模板标识" json:"template_name"`       // 模板标识
 	UpstreamID    int    `gorm:"column:upstream_id;type:int(11);default:0;NOT NULL;comment:短信通道" json:"upstream_id"`                // 短信通道
@@ -111,7 +113,7 @@ type TbAppSmsRecord struct {
 	TemplateOutID string `gorm:"column:template_out_id;type:varchar(30);default:'';NOT NULL;comment:外置模板ID" json:"template_out_id"` // 外置模板ID
 	Content       string `gorm:"column:content;type:varchar(1024);default:'';NOT NULL;comment:短信内容" json:"content"`                 // 短信内容
 	Msg           string `gorm:"column:msg;type:varchar(50);default:'';NOT NULL;comment:消息" json:"msg"`                             // 消息
-	gef.CUSD
+	mydb.CUSD
 }
 
 func (m *TbAppSmsRecord) TableName() string {
@@ -120,7 +122,7 @@ func (m *TbAppSmsRecord) TableName() string {
 
 // TbAppSmsHoldBack 应用短信防火墙
 type TbAppSmsHoldBack struct {
-	gef.ID
+	mydb.ID
 	UniAppId     int    `gorm:"column:uniapp_id;type:int(11);default:0;NOT NULL;comment:应用ID|账号所属小程序" json:"uniapp_id"`
 	RuleType     int    `gorm:"column:rule_type;type:tinyint(4);default:0;NOT NULL;comment:0单个手机号 1单个IP地址 2短信频率" json:"rule_type"` // 0单个手机号 1单个IP地址 2短信频率
 	RangeSecond  int    `gorm:"column:range_second;type:int(11);default:0;NOT NULL;comment:多少秒内" json:"range_second"`              // 多少秒内
@@ -128,7 +130,7 @@ type TbAppSmsHoldBack struct {
 	Action       int    `gorm:"column:action;type:int(11);default:0;NOT NULL;comment:执行操作，1临时冻结，2拉黑" json:"action"`                // 执行操作，1临时冻结，2拉黑
 	FrozenSecond int    `gorm:"column:frozen_second;type:int(11);default:0;NOT NULL;comment:冻结时间" json:"frozen_second"`            // 冻结时间
 	Note         string `gorm:"column:note;type:varchar(30);default:'';NOT NULL;comment:备注" json:"note"`                           // 备注
-	gef.CUSD
+	mydb.CUSD
 }
 
 func (m *TbAppSmsHoldBack) TableName() string {
@@ -137,13 +139,13 @@ func (m *TbAppSmsHoldBack) TableName() string {
 
 // TbAppSmsBlackWhite 短信黑白名单
 type TbAppSmsBlackWhite struct {
-	gef.ID
+	mydb.ID
 	UniAppId int    `gorm:"column:uniapp_id;type:int(11);default:0;NOT NULL;comment:应用ID|账号所属小程序" json:"uniapp_id"`
 	Type     int    `gorm:"column:type;type:tinyint(4);default:0;NOT NULL;comment:0黑名单 1白名单" json:"type"`            // 0黑名单 1白名单
 	RuleType int    `gorm:"column:rule_type;type:tinyint(4);default:0;NOT NULL;comment:0手机号 1IP地址" json:"rule_type"` // 0手机号 1IP地址
 	Rule     string `gorm:"column:rule;type:varchar(30);default:'';NOT NULL;comment:手机号或ip" json:"rule"`             // 手机号或ip
 	Note     string `gorm:"column:note;type:varchar(30);default:'';NOT NULL;comment:备注" json:"note"`                 // 备注
-	gef.CUSD
+	mydb.CUSD
 }
 
 func (m *TbAppSmsBlackWhite) TableName() string {
--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
 	github.com/facebookgo/grace v0.0.0-20180706040059-75cf19382434 // indirect
@@ -24,13 +23,18 @@ require (
 	github.com/gohouse/t v0.0.0-20201007094014-630049a6bfe9 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.5 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/mojocn/base64Captcha v1.3.5 // indirect
@@ -38,14 +42,19 @@ require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/tealeg/xlsx v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.37.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/mysql v1.3.6 // indirect
-	gorm.io/driver/sqlite v1.3.6 // indirect
-	gorm.io/gorm v1.23.8 // indirect
+	gorm.io/driver/postgres v1.6.0 // indirect
+	gorm.io/driver/sqlite v1.6.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
 )
 
 replace github.com/yqstech/gef v0.0.1 => ../GoEasyFrame
